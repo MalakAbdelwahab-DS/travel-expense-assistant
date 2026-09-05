28eb37b1a52f9b802d15ff453a4e8a5a6224f1cb
Malak Abdelwahab <malakabdelwahab6@gmail.com>
2026-09-05 22:50:57 +0400

    Added poetry script to run the app

--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 import uvicorn
-from router import router
+from app.router import router
 
 app = FastAPI()  # Initializing the app
 
 
+
 app.include_router(router)
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
+app.mount("/", StaticFiles(directory="app/static", html=True), name="static")
 
 
 def main():
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
 
 
 
